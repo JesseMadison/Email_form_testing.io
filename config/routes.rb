a7f80453ee3024dca2bb61bf5211b77261aab022
Alex Holder <alex.holder@bigfootds.com>
2018-05-21 16:20:38 +1000

    CRUD done the hard way.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
+
+
+
+  # resources :profiles is equivalent to all of this...
   get '/elephant' => 'people#elephant'
   get 'people/new' => 'people#new'
 
   get '/people' => 'people#index'
   post '/people' => 'people#create'
+  get '/people/:id' => 'people#show', as: 'person'
+  get '/people/:id/edit' => 'people#edit', as:'people_edit'
+
+  patch '/people/:id/' => 'people#update'
+
+  delete '/people/:id' => 'people#destroy', as: 'person_destroy'
 
 end
