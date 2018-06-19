b73ca4bdae88a6cbecafec295638bcf8fef726a8
Jesse Madison <jessemadison@hotmail.com>
2018-06-19 16:35:50 +1000

    add the profiles table via rails g scaffold Profile name:string location:string user:references

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+
+      has_one :profile
+
+      devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
 end
