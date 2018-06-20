d45060b568d7f01312104f6ba3045690df44d851
Jesse Madison <jessemadison@hotmail.com>
2018-06-20 11:09:05 +1000

    little tweaks to the view pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -28,8 +28,8 @@ class ProfilesController < ApplicationController
 
     respond_to do |format|
       if @profile.save
-        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
-        format.json { render :show, status: :created, location: @profile }
+        format.html { redirect_to root_path, notice: 'Profile was successfully created.' }
+
       else
         format.html { render :new }
         format.json { render json: @profile.errors, status: :unprocessable_entity }
@@ -42,8 +42,8 @@ class ProfilesController < ApplicationController
   def update
     respond_to do |format|
       if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
-        format.json { render :show, status: :ok, location: @profile }
+        format.html { redirect_to root_path, notice: 'Profile was successfully updated.' }
+
       else
         format.html { render :edit }
         format.json { render json: @profile.errors, status: :unprocessable_entity }
